a5fd5345c07243f3446eff3f628ff1bd5cac6f73
Tobi Adeniji <adenijitobi94@gmail.com>
2024-11-08 12:59:47 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 
 module.exports = {
-    PORT: process.env.PORT,
+    PORT: process.env.PORT || 5000,
     MONGODB_URL: process.env.MONGODB_URL,
     SECRET_KEY:process.env.SECRET_KEY
 }
